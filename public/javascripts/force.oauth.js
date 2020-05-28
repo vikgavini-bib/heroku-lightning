9e6176ce70947d864eee706abf7378774584b46d
Vik Gavini <vikram.gavini1_ext@michelin.com>
2020-05-28 06:20:55 -0700

    Update force.oauth.js

--- public/javascripts/force.oauth.js
+++ public/javascripts/force.oauth.js
@@ -8,7 +8,7 @@ var force = (function () {
 
     // The login url
     //var loginUrl = 'https://login.salesforce.com',
-    var loginUrl = 'https://vgxred-dev-ed.my.salesforce.com',
+    var loginUrl = 'https://test.salesforce.com',
 
     // The instance url, needed until pattern for my domains, etc. is determined
         instanceUrl = null,
