b7ae3f0e285b7c9e9efc94cfb69cbec9e38cd85b
Vik Gavini <vikram.gavini1_ext@michelin.com>
2020-05-28 06:57:30 -0700

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -34,14 +34,33 @@ function setupLightning(callback) {
 	    // Transform the URL for Lightning
 	    var url = oauth.instanceUrl.replace("my.salesforce", "lightning.force");
 
-	    $Lightning.use(appName, 
+	    /*$Lightning.use(appName, 
 	        function() {
 				_lightningReady = true;
 				document.getElementById("chatterFeedButton").style.display = "";
 				if (typeof callback === "function") {
 					callback();
 				}
-	        }, url, oauth.access_token);
+	        }, url, oauth.access_token);*/
+    	$Lightning.use("c:LightningOutAuthenticated",    // name of the Lightning app
+        function() {                  // Callback once framework and app loaded
+        $Lightning.createComponent(
+            "c:LightningOutCard", // top-level component of your app
+            {
+                title: "TEST AUTH",
+                contentBody: "BODY_TEST_CONTENT"
+            },                  // attributes to set on the component when created
+            "lightningLocator",   // the DOM location to insert the component
+            function (cmp) {
+                // callback when component is created and active on the page
+                console.log('Lightning Out App has loaded!');
+            }
+        );
+        },
+        'https://pocmobile-sgws.cs97.force.com',  // Community endpoint
+	 oauth.access_token
+		       
+    );		
 	}
 }
 
